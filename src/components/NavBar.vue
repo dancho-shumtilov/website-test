1ec39c3a8765bd912d746465a5e7492aaa42290d
dancho-shumtilov <zombayo@gmail.com>
2021-03-12 23:31:18 +0200

    My Website commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1><img src='../assets/loggo.png' alt="my logo"/></h1>    
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello, my name is Dancho Petrov</h1>
+    <p>I'm the front end developer</p>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
